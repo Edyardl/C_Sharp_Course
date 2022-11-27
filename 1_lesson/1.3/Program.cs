5403cebfe13586a088de4917274b701fb450350b
Eduard <Edyardl@mail.ru>
2022-11-27 23:40:37 +0300

    Task

---
+++ 1_lesson/1.3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Введите число: ");
+String w = Console.ReadLine();
+int N = int.Parse(w);
+int NetN =- N;
+
+while(NetN <= N)
+{
+    Console.Write($"{NetN} ");
+    NetN +=1;
+}
---
+++ 1_lesson/1.4/Program.cs
@@ -0,0 +1,6 @@
+﻿//  Напишите программу вычисления модуля числа.
+
+Console.WriteLine("Введите число: ");
+var t = Console.ReadLine();
+int N = t == null ? 0 : int.Parse(t);
+Console.WriteLine($"{Math.Abs(N)}");
