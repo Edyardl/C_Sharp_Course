29fc250df223a76a3dd271d63ccfff8ebcf160a9
Eduard <Edyardl@mail.ru>
2022-11-28 18:35:20 +0300

    DZ task 3

---
+++ 1_lesson/dz/3.0/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+
+int b_1 = int.Parse(Console.ReadLine());
+
+if (b_1 % 2 ==0)
+{
+    Console.Write($"The number {b_1} is even");
+}
+else
+{
+    Console.Write($"The number {b_1} is odd");
+}
