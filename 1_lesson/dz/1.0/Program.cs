c8ebc5b04e9f2cc7d1778ffe95f7df10f85d442b
Eduard <Edyardl@mail.ru>
2022-11-28 18:33:37 +0300

    DZ task 2

---
+++ 1_lesson/dz/1.0/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+
+
+string a_b_1 = Console.ReadLine();
+string a_b_2 = Console.ReadLine();
+
+int b_1 = int.Parse(a_b_1);
+int b_2 = int.Parse(a_b_2);
+
+if (b_1>b_2)
+{
+    Console.Write($"The number {b_1} is greater than {b_2}");
+}
+else if (b_1 == b_2)
+{
+    Console.Write($"The number {b_1} is equal to the number {b_2}");
+}
+else
+{
+    Console.Write($"The number {b_2} is greater than {b_1}");
+}
---
+++ 1_lesson/dz/2.0/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+
+int b_1 = int.Parse(Console.ReadLine());
+int b_2 = int.Parse(Console.ReadLine());
+int b_3 = int.Parse(Console.ReadLine());
+
+if (b_1<b_2)
+{
+    b_1 = b_2;
+}
+if (b_1<b_3)
+{
+    b_1 = b_3;
+}
+Console.Write($"The number {b_1} is the largest!");
